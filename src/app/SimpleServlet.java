9371ae05e9d6b5f96063e294dc01a94750358eca
junho876 <junhocom876@gmail.com>
2018-10-04 18:07:53 +0900

    first commit

---
+++ src/app/SimpleServlet.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MyServlet", urlPatterns = {"/hello"})
+public class SimpleServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        ServletOutputStream out = res.getOutputStream();
+        out.write("HelloWorld".getBytes());
+        out.flush();
+        out.close();
+    }
+}
+
+
